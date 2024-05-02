3ee452bad4081f9b8f22cc5dcf7bff4fd04f2152
Amberpatton007 <amberpatton007@gmail.com>
2024-05-01 19:37:35 -0700

    Homework assignment 1

---
+++ scripts/apps.js
@@ -0,0 +1,21 @@
+function sayHello(){
+    console.log("Hello there");
+    
+}
+
+function sayGoodbye(name,lastname){
+console.log("Adios!"+name+""+lastname);
+var color = "blue";
+}
+
+function init(){
+    console.log("hello world");
+    //if the user pays they can visit
+    //{
+        sayHello();
+        //}
+}
+
+window.onload = init;//<-- this is a function, and will be executed 
+
+//what is an init?
