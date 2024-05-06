50d68d412ac090838375277aac611f76d77b117e
Amberpatton007 <amberpatton007@gmail.com>
2024-05-06 16:56:16 -0700

    "second homework assignment"

--- scripts/apps.js
+++ scripts/apps.js
@@ -1,19 +1,29 @@
-function sayHello(){
-    console.log("Hello there");
-    
+function saveTask(){
+console.log("saving task");
+//get values
+const title =$("#txtTitle").val();
+const desc =$("txtDescription").val();
+const color =$("#selColor").val();
+const date =$("#selDate").val();
+const status =$("#selStatus").val();
+const budget =$("#numBudget").val();
+console.log(title, desc,color,date,status,budget);
+//build an object
+
+//save to server
+
+//display the task
+
 }
 
-function sayGoodbye(name,lastname){
-console.log("Adios!"+name+""+lastname);
-var color = "blue";
-}
-
+//i=i+1;
+//i+=1;
 function init(){
-    console.log("hello world");
-    //if the user pays they can visit
-    //{
-        sayHello();
-        //}
+
+    //load data
+
+    //hook events
+$("#btnSave").click(saveTask)
 }
 
 window.onload = init;//<-- this is a function, and will be executed 
