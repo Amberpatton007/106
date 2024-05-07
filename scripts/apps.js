21c9f62a38f43fbf6e6c8403ef5c6439a8570df1
Amberpatton007 <amberpatton007@gmail.com>
2024-05-06 19:16:01 -0700

    Class 3

--- scripts/apps.js
+++ scripts/apps.js
@@ -1,6 +1,6 @@
 function saveTask(){
 console.log("saving task");
-//get values
+//get values. The values come from http. They're attributes
 const title =$("#txtTitle").val();
 const desc =$("txtDescription").val();
 const color =$("#selColor").val();
@@ -8,16 +8,52 @@ const date =$("#selDate").val();
 const status =$("#selStatus").val();
 const budget =$("#numBudget").val();
 console.log(title, desc,color,date,status,budget);
-//build an object
 
+//build an object using the attributes from above
+
+let taskToSave = new Task (title,desc,color,date,status,budget);
+console.log(taskToSave);
+displayTask(taskToSave);
 //save to server
 
 //display the task
+function displayTask(task){
+
+    let syntax = 
+    `<div class="task" style="Border-color: ${task.color};">
+    <div class="info">
+
+    <h3>${task.title}</h3>
+    <p>${task.description}</p>
+    </div>
+    <label class="status">${task.status}</label>
+    <div class="date-budget">
+        <label>${task.date}</label>
+        <label>${task.budget}</label>
+    </div>
+    `;
+    $(".pending-task").append(syntax);
+}
+
 
 }
 
 //i=i+1;
 //i+=1;
+
+function testReq()
+{
+    $.ajax({
+        type: "delete",
+        url:"http://fsdiapi.azurewebsites.net",
+        success: function(response) {
+            console.log(response);
+        },
+        error: function(error) {
+            console.log(error)
+        }
+    });
+}
 function init(){
 
     //load data
