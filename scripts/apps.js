5bb4e9f2060e00ee0d0852611960032be1ab2226
Amberpatton007 <amberpatton007@gmail.com>
2024-05-18 11:58:32 -0700

    latest assignment

--- scripts/apps.js
+++ scripts/apps.js
@@ -2,41 +2,83 @@ function saveTask(){
 console.log("saving task");
 //get values. The values come from http. They're attributes
 const title =$("#txtTitle").val();
-const desc =$("txtDescription").val();
+const description =$("txtDescription").val();
 const color =$("#selColor").val();
 const date =$("#selDate").val();
 const status =$("#selStatus").val();
 const budget =$("#numBudget").val();
-console.log(title, desc,color,date,status,budget);
+console.log(title,description,color,date,status,budget);
 
 //build an object using the attributes from above
 
-let taskToSave = new Task (title,desc,color,date,status,budget);
+let taskToSave = new Task (title,description,color,date,status,budget);
 console.log(taskToSave);
-displayTask(taskToSave);
+
 //save to server
 
-//display the task
-function displayTask(task){
+    $.ajax({
+        type: "POST",
+        url:"http://fsdiapi.azurewebsites.net/api/tasks/",
+        data: JSON.stringify(taskToSave),
+        contentType: "application/json",
+        success: function(res) {
+            console.log(res);
+            //displayTask(taskToSave);
+            },
+        error: function(error) {
+            console.log(error);
+        }
+});
 
+
+
+
+}
+
+//display the task
+
+function displayTask(task){
+    
     let syntax = 
     `<div class="task" style="Border-color: ${task.color};">
     <div class="info">
-
+    
     <h3>${task.title}</h3>
     <p>${task.description}</p>
     </div>
     <label class="status">${task.status}</label>
     <div class="date-budget">
-        <label>${task.date}</label>
-        <label>${task.budget}</label>
+    <label>${task.date}</label>
+    <label>${task.budget}</label>
     </div>
     `;
     $(".pending-task").append(syntax);
 }
 
+function loadTask(){
+    $.ajax({
+        type: "GET",
+        url:"http://fsdiapi.azurewebsites.net/api/tasks",
+        success: function(response) {
 
+            let data = JSON.parse(response);
+            console.log(data);
+            for(let i=0; i<data.length; i++)
+            {   let task = data[i];
+                if(task.name == "Amber")
+            {
+            displayTask(task)
+            }
+        }
+            displayTask(data);
+            console.log(response);
+        },
+        error: function(error) {
+            console.log(error)
+        }
+});
 }
+
 
 //i=i+1;
 //i+=1;
@@ -44,7 +86,7 @@ function displayTask(task){
 function testReq()
 {
     $.ajax({
-        type: "delete",
+        type: "GET",
         url:"http://fsdiapi.azurewebsites.net",
         success: function(response) {
             console.log(response);
@@ -54,12 +96,15 @@ function testReq()
         }
     });
 }
+
+
 function init(){
 
     //load data
 
     //hook events
 $("#btnSave").click(saveTask)
+loadTask();
 }
 
 window.onload = init;//<-- this is a function, and will be executed 
--- scripts/task.js
+++ scripts/task.js
@@ -1,8 +1,8 @@
 class Task {
-    constructor(title,desc,color,date,status,budget)
+    constructor(title,description,color,date,status,budget)
     {
         this.title = title;
-        this.desc = desc;
+        this.description = description;
         this.color = color;
         this.date = date;
         this.status = status;
